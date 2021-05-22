597be79f13dfd3d41772a480f9042254b569f35c
0B1t322 <dan.demin@gmail.com>
2021-05-22 11:03:30 +0300

    server.go

--- src/service/server/server.go
+++ src/service/server/server.go
@@ -2,16 +2,18 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"sync"
 	"time"
 
 	model "github.com/RTUITLab/Civfrovoi-Proriv-Back/pkg/models/coordinates"
 	pb "github.com/RTUITLab/Civfrovoi-Proriv-Back/protos/coordservice"
 	"github.com/segmentio/ksuid"
 	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
-	"github.com/streadway/amqp"
 )
 
 type Config struct {
@@ -20,8 +22,8 @@ type Config struct {
 
 type Server struct {
 	pb.UnimplementedCoordsServiceServer
-	DB *gorm.DB
-	RabbitConn	*amqp.Connection
+	DB 				*gorm.DB
+	RabbitConn		*amqp.Connection
 	GetCoordTime	time.Duration
 }
 
@@ -118,29 +120,440 @@ func(s *Server) UpdateUnits(ctx context.Context, req *pb.UpdateUnitsReq) (*pb.Up
 	return resp, nil
 }
 func(s *Server)	SweepingUp(ctx context.Context, req *pb.OpeataionOn) (*pb.Empty, error){
-	panic("Not imolemented")
+	logger := prepareEntrty("SweepingUp")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Sweeping Up",
+				From: req.Coords,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 func(s *Server)	ReagentTreatment(ctx context.Context, req *pb.OpeataionOn) (*pb.Empty, error){
-	panic("Not imolemented")
+	logger := prepareEntrty("ReagentTreatment")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Reagent Treatment",
+				From: req.Coords,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 func(s *Server)	ShaftFormation(ctx context.Context, req *pb.OpeataionOn) (*pb.Empty, error){
-	panic("Not imolemented")
+	logger := prepareEntrty("ShaftFormation")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Shaft Formation",
+				From: req.Coords,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 func(s *Server)	MoveSnowToTemp(ctx context.Context, req *pb.OperationFromTo) (*pb.Empty, error) {
-	panic("Not imolemented")
+	logger := prepareEntrty("MoveSnowToTemp")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Move Snow To Temp",
+				From: req.From,
+				To: req.To,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 func(s *Server)	LoadingSnowFromTemp(ctx context.Context, req *pb.OpeataionOn) (*pb.Empty, error) {
-	panic("Not imolemented")
+	logger := prepareEntrty("LoadingSnowFromTemp")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Loading Snow From Temp",
+				From: req.Coords,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 func(s *Server)	ExportSnowFromTemp(ctx context.Context, req  *pb.OperationFromTo) (*pb.Empty, error) {
-	panic("Not imolemented")
+	logger := prepareEntrty("ExportSnowFromTemp")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Export Snow From Temp",
+				From: req.From,
+				To: req.To,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 func(s *Server)	ClearTemp(ctx context.Context, req *pb.OpeataionOn) (*pb.Empty, error) {
-	panic("Not imolemented")
+	logger := prepareEntrty("ClearTemp")
+	channel, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to open channle on rabbit mq", err)
+		return nil, status.Error(codes.Internal, "Faield to send operation")
+	}
+	defer channel.Close()
+
+	wg := sync.WaitGroup{}
+	for _, u := range req.Units.Ids {
+		wg.Add(1)
+		go func(id string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			q, err := channel.QueueDeclare(
+				id,
+				true,
+				false,
+				false,
+				true,
+				nil,	
+			)
+			if err != nil {
+				logger.Errorf("Failed to open queue for %s: %v", id, err)
+				return
+			}
+			ops := &pb.Operaions{
+				OperationName: "Clear Temp",
+				From: req.Coords,
+			}
+			data, err := json.Marshal(ops)
+			if err != nil {
+				logger.Error("Failed to marshall: ", err)
+				return
+			}
+
+			err = channel.Publish(
+				"",
+				q.Name,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body: data,
+				},
+			)
+			if err != nil {
+				logger.Error("Failed to send to AMQP: ", err)
+			}
+		}(u, &wg)
+	}
+	wg.Wait()
+
+	return &pb.Empty{}, status.Error(codes.OK, "")
 }
 
 func(s *Server)	ListenCommands(req *pb.Unit, resp pb.CoordsService_ListenCommandsServer) error{
-	panic("Not imolemented")
+	logger := prepareEntrty("ListenCommands")
+	ch, err := s.RabbitConn.Channel()
+	if err != nil {
+		logger.Error("Faield to get chan: ", err)
+		return status.Error(codes.Internal, "")
+	}
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		req.Id,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		logger.Error("Faield to consume: ", err)
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	for d := range msgs {
+		ops := &pb.Operaions{}
+		err := json.Unmarshal(d.Body, ops)
+		if err != nil {
+			logger.Error("Faield to unmarhsall: ", err)
+			// TODO think about error handling
+			continue
+		}
+
+		err = resp.Send(ops)
+		if stats, ok := status.FromError(err); ok && stats.Code() == codes.Canceled {
+			logger.Info("Cancelen")
+			return nil
+		} else if ok && stats.Code() == codes.DeadlineExceeded {
+			logger.Info("Deadline")
+			return nil
+		}else if err != nil {
+			logger.Error("Error on streaming", err)
+			return status.Error(codes.Aborted, "")
+		}
+	}
+
+	return nil
 }
 
 func(s *Server) InitApp(ctx context.Context, req *pb.InitReq) (*pb.ID, error) {
